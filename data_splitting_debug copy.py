4cf488b4b98c11bca090b8a77111bee0956cb983
ToshaQE <uceiap2@ucl.ac.uk>
2022-05-30 00:44:45 +0100

    fixing index allignment

--- data_splitting_debug copy.py
+++ data_splitting_debug copy.py
@@ -42,7 +42,7 @@ def algo(df, target, max_lag):
     feature_df = df.loc[:, ~df.columns.isin([target, "Date"])]
     target_df = df.loc[:, target]
 
-    X_train, X_test, y_train, y_test = train_test_split(feature_df, target_df, test_size=0.4, shuffle=False) 
+    X_train, X_test, y_train, y_test = train_test_split(feature_df, target_df, test_size=0.01, shuffle=False) 
 
     # Step 2: Building a univariate model and finding the optimal l
     BICs = []
@@ -55,15 +55,32 @@ def algo(df, target, max_lag):
     # model = AutoReg(df_small.iloc[:,1], lags=min_bic_ind).fit()
     # model.summary()
 
+    # Due to statsmodels weird properties, you can not test trained model on unseen y-data, but only on unseen X-data.
+    # Hence we need to perform some data manipulations to make the testing possible.
+
+    columns_y = []
+    for i in list(range(1, min_bic_ind+1)):
+        columns_y.append(target+".L"+str(i))
+
+    y_lags_df = pd.DataFrame(columns=columns_y)
+    for i in list(range(min_bic_ind)):
+        y_lags_df[columns_y[i]] = y_train.shift(i+1)
+
+    # NAs filled with are later automatically truncated by the AutoReg
+    y_lags_df.fillna(1, inplace=True)
+    y_lags_df = y_lags_df.iloc[max_lag:,:]
 
     # Step 2: Bulding augmented model and finding the optimal w for each Xi
     
     Xs = list(X_train.columns)
 
+    # Trunctaing y_train for model training at max_lag length
+    y_train_m = y_train.iloc[max_lag:]
+    y_train_m.reset_index(drop=True, inplace=True)
     # Defining dictionary to store all augmented models
     aug_models = {}
     feature_n_dfs = {}
-    feature_n_dfs_merge = []
+    feature_n_dfs_merge = [y_lags_df]
     
     for n in list(range(len(Xs))):
         columns = []
@@ -77,27 +94,32 @@ def algo(df, target, max_lag):
         # NAs filled with are later automatically truncated by the AutoReg
         feature_n_df.fillna(1, inplace=True)
 
+        feature_n_df = feature_n_df.iloc[max_lag:,:]
+        y_and_x_lags_df = pd.concat([y_lags_df, feature_n_df], axis=1)
 
-        # AutoReg only cuts off the 1s for laags <= min_bic_ind - FIXXX
         BICs = []
         #Why do I have max_lag-1 and then i+1?
+        # +1 is to not make X lags = 0
+        # y_and_x_lags_df_m = y_and_x_lags_df.iloc[:,:i+len(list(y_lags_df.columns))+1]
+        y_and_x_lags_df.reset_index(drop=True, inplace=True)
         for i in list(range(max_lag-1)):
-            model = AutoReg(y_train, lags=min_bic_ind, exog=feature_n_df.iloc[:,:i+1]).fit()
+            model = AutoReg(y_train_m, lags=0, exog=y_and_x_lags_df.iloc[:,:i+len(list(y_lags_df.columns))+1]).fit()
             BICs.append(model.bic)
 
         min_bic_ind_aug = BICs.index(min(BICs))
         #Full and Partial autocorrelation plot?
-        feature_n_df1 = feature_n_df
-        feature_n_df = feature_n_df.iloc[:,:min_bic_ind_aug+1]
+        feature_n_df1 = y_and_x_lags_df
+        y_and_x_lags_df = y_and_x_lags_df.iloc[:,:min_bic_ind_aug+len(list(y_lags_df.columns))+1]
+        y_and_x_lags_df.reset_index(drop=True, inplace=True)
 
-        model = AutoReg(y_train, lags=min_bic_ind, exog=feature_n_df).fit()
+        model = AutoReg(y_train_m, lags=0, exog=y_and_x_lags_df).fit()
 
         gr_test_df = pd.concat([X_train[Xs[n]], y_train], axis=1)
         granger_p_stat = grangercausalitytests(gr_test_df, maxlag=[min_bic_ind_aug+1])[min_bic_ind_aug+1][0]['params_ftest'][1]
         if granger_p_stat <= 0.05:
             aug_models[Xs[n]] = model
             feature_n_dfs[Xs[n]] = feature_n_df1
-            feature_n_dfs_merge.append(feature_n_df)
+            feature_n_dfs_merge.append(y_and_x_lags_df.iloc[:,len(list(y_lags_df.columns)):])
             #model.summary()
         elif granger_p_stat <= 0.1:
             print(f'\n\nGranger causality from "{target}" to "{Xs[n]}" is rejected with a p-value={granger_p_stat:.3}')
@@ -113,21 +135,7 @@ def algo(df, target, max_lag):
     try:
         feature_n_dfs_merge = pd.concat(feature_n_dfs_merge, axis=1)
 
-        # Due to statsmodels weird properties, you can not test trained model on unseen y-data, but only on unseen X-data.
-        # Hence we need to perform some data manipulations to make the testing possible.
-
-        # for n in list(range(len(Xs))):
-        # columns = []
-        # for i in list(range(1, max_lag+1)):
-        #     columns.append(Xs[n]+".L"+str(i))
-
-        # feature_n_df = pd.DataFrame(columns=columns)
-        # for i in list(range(max_lag)):
-        #     feature_n_df[columns[i]] = X_train[Xs[n]].shift(i+1)
-
-        # feature_n_df.fillna(1, inplace=True)
-
-        fin_model = AutoReg(y_train, lags=min_bic_ind, exog=feature_n_dfs_merge).fit()
+        fin_model = AutoReg(y_train_m, lags=0, exog=feature_n_dfs_merge).fit()
 
         MAE = np.nanmean(abs(fin_model.predict() - y_train))
 
@@ -143,3 +151,4 @@ fin_model, aug_models, dfs, dfs_merged, MAE = algo(df=df_small, target="Close", 
 print(fin_model.summary())
 
 print(MAE)
+dfs_merged
