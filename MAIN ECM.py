8290bf96c75e2c92ba416c2b807fa3bf6e17bc90
apoletaev <qepolet@gmail.com>
2022-06-30 18:54:35 +0100

    Backtransformer started

--- MAIN ECM.py
+++ MAIN ECM.py
@@ -474,7 +474,11 @@ msft_pmd_df = msft_pmd_df.iloc[:,:-1]
 
 #fin_model, aug_models, dfs, dfs_merged, MAE, Model = algo(df=df_medium, target="Close", max_lag=20)
 
-Model_Data = algo(df=aapl_medium, target="Close", max_lag=20, stationarity_method = 0, test_size=0.2)
+Model_Data = algo(df=aapl_long, target="Close", max_lag=20, stationarity_method = 1, test_size=0.05)
+
+apple_stat = pd.concat([Model_Data.train_y, Model_Data.train_x], axis=1)
+apple_stat.to_csv("aaple_stat.csv", index=True)
+
 
 print(Model_Data.summary)
 
